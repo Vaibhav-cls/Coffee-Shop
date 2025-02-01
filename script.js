98bc4d4985ef144cc94099cc19513d3aa6a46f4e
Vaibhav Ghanekar <63966372+Vaibhav-cls@users.noreply.github.com>
2025-02-01 22:06:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let image = document.getElementById("hd-img");
+window.addEventListener('scroll', function () {
+  var navbar = document.querySelector('header');
+  let nav_img = document.querySelector('img');
+  let img_t = document.querySelector('span');
+  let up_arrow = document.querySelector('div');
+  navbar.classList.toggle('scrolled', window.scrollY > 0);
+  nav_img.classList.toggle('sc-nav', window.scrollY > 0);
+  img_t.classList.toggle('sc-title', window.scrollY > 0);
+  up_arrow.classList.toggle('sc-arrow', window.scrollY > 0);
+});
+
+let menu = document.getElementById("dropbox");
+let dropdown = document.getElementById("sidebar");
+let cross = document.getElementById("close");
+
+function showBox() {
+  dropdown.style.left = "0px";
+  dropdown.style.display = "block";
+}
+function hideBox() {
+  dropdown.style.left = "-415px";
+  dropdown.style.display = "none";
+}
+function prompt() {
+  alert("Currently working on it!!");
+}
+
